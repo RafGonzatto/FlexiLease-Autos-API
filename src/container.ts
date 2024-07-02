29d802e5bed39ecec7a35bca4dd828d1d3f91a3d
RafGonzatto <rafaelgonzatto181@gmail.com>
2024-07-02 13:16:13 -0300

    feat: implementar container do tsyringe

---
+++ src/container.ts
@@ -0,0 +1,21 @@
+import { container } from 'tsyringe'
+// import IReserveRepository from './repositories.interfaces/reserve.repository.interface'
+// import ReserveRepository from './repositories/reserve.repository'
+import { ICarRepository } from './repositories.interfaces/car.repository.interface'
+import CarRepository from './repositories/car.repository'
+import { IUserRepository } from './repositories.interfaces/user.repository.interface'
+import UserRepository from './repositories/user.repository'
+
+// container.registerSingleton<IReserveRepository>(
+//   'ReserveRepository',
+//   ReserveRepository,
+// )
+
+container.registerSingleton<ICarRepository>(
+  'CarRepository',
+  CarRepository,
+)
+container.registerSingleton<IUserRepository>(
+  'UserRepository',
+  UserRepository,
+)
